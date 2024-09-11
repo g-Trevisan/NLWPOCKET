8bbd3e74551fc431a122532ae1ee3702beaa1338
g-Trevisan <gtrevis15@gmail.com>
2024-09-10 22:40:38 -0300

    fundamentos 04 - while

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-// hello world - comentario com barra barra
-// console.log("hello world")
-
-const { checkboxClasses } = require("@mui/material")
-
-let meta = {
-    value: 'ler um livro por mês',
-    checked: false,
-    log: (info) => {
-        console.log(info)
+const start = () => {
+    let count = 0
+    while(count <= 10){
+        console.log(count)
+        count += 1
     }
 }
 
-let metas = [
-    meta,
-    {
-        value: "caminhar 20 minutos todos os dias",
-        checked: false
-    }
-]
-
-console.log(meta.value)
-console.log(metas[1].value)
+start()
